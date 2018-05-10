4eef38659fecb92329666ca59230cb35f442ba4b
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-05-10 16:40:54 -0300

    Creating paste model

--- pastebin/pastebin/models.py
+++ pastebin/pastebin/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+
+
+class Paste(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    language = models.CharField(choices=LANGUAGE_CHOICES,
+                                default='python', max_length=100)
